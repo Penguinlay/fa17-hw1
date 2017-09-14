a87bccb19963eb32d6b616ffc816e5228e3fed1a
Shein Lin Phyo <sheinlinphyo@gmail.com>
2017-09-13 21:05:22 -0700

    updated some variable names and added comments

--- foobar.rb
+++ foobar.rb
@@ -1,10 +1,21 @@
-class Foobar
+=begin
+File Name: foobar.rb
+Name: Penguinlay
+Assignment: HW1
+Course: CS 198 - Ruby on Rails - Fall 2017
+Start: 2017091303
+End: 2017091303
+=end
 
-  def self.baz(a)
-    b = Array.new
-    g = 0
-    a.each{|c| b.push(c.to_i + 2)}
-    b.keep_if{|d| d % 2 == 0}.uniq.delete_if{|e| e > 10}.each{|f| g += f}
-    return g
+class Foobar
+    def self.baz(a)
+        # Convert array of number from string to number and add 2 to each.
+        # Then, keep the even, remove duplicate, remove number above 10.
+        # Finally, get the sum of remaining numbers.
+        b = Array.new
+        s = 0
+        a.each{|t| b.push(t.to_i + 2)}
+        b.keep_if{|t| t % 2 == 0}.uniq.delete_if{|t| t > 10}.each{|t| s += t}
+    return s
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,18 +1,35 @@
+=begin
+File Name: hw1.rb
+Name: Penguinlay
+Assignment: HW1
+Course: CS 198 - Ruby on Rails - Fall 2017
+Start: 2017091303
+End: 2017091303
+=end
+
+# Question 1
 def squared_sum(a, b)
+    # Square the sum of inputs.
     return (a + b)**2
 end
 
+# Question 2
 def sort_array_plus_one(a)
+    # Sort and add 1 to each number in input array.
     b = Array.new
-    a.sort!.each{|c| b.push(c + 1)}
+    a.sort!.each{|t| b.push(t + 1)}
     return b
 end
 
+# Question 3
 def combine_name(first_name, last_name)
+    # Print first and last name seprated by a space.
     return "#{first_name} #{last_name}"
 end
 
+# Question 4
 def blockin_time(a)
+    # Get class file and call function baz with array of string as input.
     require './foobar'
     Foobar.baz a
 end
