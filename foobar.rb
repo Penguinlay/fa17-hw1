372c6b00bf0809fd8e79ec273f905fb0996f55fe
Shein Lin Phyo <sheinlinphyo@gmail.com>
2017-09-13 19:52:42 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,10 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    b = Array.new
+    g = 0
+    a.each{|c| b.push(c.to_i + 2)}
+    b.keep_if{|d| d % 2 == 0}.uniq.delete_if{|e| e > 10}.each{|f| g += f}
+    return g
   end
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,20 +1,18 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+    return (a + b)**2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+    b = Array.new
+    a.sort!.each{|c| b.push(c + 1)}
+    return b
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+    return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+    require './foobar'
+    Foobar.baz a
 end
